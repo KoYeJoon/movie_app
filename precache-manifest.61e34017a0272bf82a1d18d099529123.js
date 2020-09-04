a1559c5cf58948a198d05f110e0b0abc2ade1a2b
yejoon <yejoon.ko@gmail.com>
2020-09-05 00:04:28 +0900

    Updates

---
+++ precache-manifest.61e34017a0272bf82a1d18d099529123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b87460d0e1c2db3137cc41e36bc10fa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fdb683487aa2eaf3675c",
+    "url": "/movie_app/static/css/main.1f6e981f.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb683487aa2eaf3675c",
+    "url": "/movie_app/static/js/main.7bf73c58.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
